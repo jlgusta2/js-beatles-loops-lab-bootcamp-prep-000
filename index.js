3379e17c2b5932fefc23742b84d368eb6c4f1ce0
jennifer gustavson <jengustavson@gmail.com>
2018-05-29 00:26:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay (musicians,instruments){
+  var empty = [];
+  for (var i = 0; i < musicians.length; i++){
+    empty.push(musicians[i] + " plays " +  instruments[i]);
+}
+    
+    return empty;
+}
+    
+function johnLennonFacts (facts)
